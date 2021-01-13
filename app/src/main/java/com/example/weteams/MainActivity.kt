46209ac085be0c9967b879d1d0f90c7a59774cd7
Takanori Hoshi <takanori.hoshi@gmail.com>
2021-01-13 06:30:13 -0800

    Setup Jetpack Compose

--- app/src/main/java/com/example/weteams/MainActivity.kt
+++ app/src/main/java/com/example/weteams/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.weteams
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weteams.ui.theme.WeTeamsTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            WeTeamsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeTeamsTheme {
+        Greeting("Android")
     }
 }
